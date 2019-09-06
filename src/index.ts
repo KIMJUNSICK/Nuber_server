f30dc563c9375ea72493aa3191cbbcee0d9e69a6
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-06 13:44:25 +0900

    App Options & Start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("works");
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+require("dotenv").config();
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+console.log(PORT);
+
+const appOptions: Options = {
+  port: PORT,
+  endpoint: GRAPHQL_ENDPOINT,
+  playground: PLAYGROUND
+};
+
+const handleAppStart = () =>
+  console.log(`Listening on:✅  http://localhost:${PORT}`);
+
+app.start(appOptions, handleAppStart);
