83656ed6d6a7334a764bd7706ade130f0d975cec
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-23 21:50:30 +0900

    SendChatMessage resolver

---
+++ src/api/Chat/SendChatMessage/SendChatMessage.resolvers.ts
@@ -0,0 +1,59 @@
+import {
+  SendChatMessageMutaitionArgs,
+  SendChatMessageResponse
+} from "src/types/graph";
+import { Resolvers } from "src/types/resolvers";
+import Chat from "../../../entities/Chat";
+import Message from "../../../entities/Message";
+import User from "../../../entities/User";
+import { isAuthenticated } from "../../../utils/isAuthenticated";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    SendChatMessage: async (
+      _,
+      args: SendChatMessageMutaitionArgs,
+      { req }
+    ): Promise<SendChatMessageResponse> => {
+      isAuthenticated(req);
+      const user: User = req.user;
+      try {
+        const chat = await Chat.findOne({ id: args.chatId });
+        if (chat) {
+          if (chat.driverId === user.id || chat.passengerId === user.id) {
+            const message = await Message.create({
+              text: args.text,
+              user,
+              chat
+            }).save();
+            return {
+              ok: true,
+              error: null,
+              message
+            };
+          } else {
+            return {
+              ok: false,
+              error: "Unauthorized",
+              message: null
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Chat not found",
+            message: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          message: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
